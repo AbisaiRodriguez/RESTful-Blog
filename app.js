96111b37e9883ef2842dcacc41625ceda4fcf224
Abisai Rodriguez <abisai.rodriguez13@gmail.com>
2020-02-13 19:33:20 -0800

    Delete route was added

--- app.js
+++ app.js
@@ -93,6 +93,19 @@ app.put("/blogs/:id", function(req, res){
     });
 });
 
+//DELETE ROUTE
+app.delete("/blogs/:id", function(req, res){
+    //destroy blog
+    Blog.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            res.redirect("/blogs");
+        }
+        else{
+            res.redirect("/blogs");
+        }
+    });
+});
+
 app.listen(3000, function(){
     console.log("Server is Running");
 });
